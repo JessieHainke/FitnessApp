4b78ebc2fbcfd06a17d5df55ed52bcf6f3446e1f
Jessica Hainke <j-k.hainke@t-online.de>
2023-04-27 06:47:04 +0200

    Browse-Seite kaputt gemacht und wieder halb repariert - Idee zu Verkürzung des Codes, funktioniert aber nur halb; außerdem eines der Programme in Button verwandelt, der aufgerufen wird, aber das Overlay des ausgewählten Workouts noch nicht anzeigt. Grmpf.

--- src/App/Browse.jsx
+++ src/App/Browse.jsx
@@ -1,12 +1,34 @@
 import DefaultLayout from "../Layouts/DefaultLayout";
+import Program from "./Program";
 
 export default function Browse() {
+  
+  function Programs( {className, children} ) {
+  {/*function Title( {className, children}) {
+              <h2 className="text-2xl font-bold">${className}test</h2>
+              
+              return (
+                <Title className={`bg-red ${children}`}/>
+              
+                <div className={`bg-gradient-to-br  pt-16 rounded-2xl px-4 py-3 shadow-white h-48 text-center ${className}`}>
+                    {children}
+                    <Title />
+                </div>)
+            }
+        } */}
+        
+  }
+
   return (
     <DefaultLayout>
-      <h2 className="text-2xl font-bold">Browse</h2>
-      <div className="bg-gradient-to-br from-orange to-pink pt-16 rounded-2xl px-4 py-3 shadow-white h-48 text-center">
-        <h2 className="text-2xl font-bold">Titel des Programms</h2>
+      <div>
+      {/*<Programs className="bg-gradient-to-br from-orange to-pink ${className}" />
+      <Programs className="bg-gradient-to-br from-cyan to-yellowgreen ${className} {`<Title />`}" />*/}
       </div>
+      <h2 className="text-2xl font-bold" >Browse</h2>
+      <button className="bg-gradient-to-br from-orange to-pink pt-16 rounded-2xl px-4 py-3 shadow-white h-48 text-center" onClick={clickHandler}>
+        <h2 className="text-2xl font-bold">Titel des Programms</h2>
+      </button>
       <div className="bg-gradient-to-br from-cyan to-yellowgreen pt-16 rounded-2xl px-4 py-3 shadow-white h-48 text-center">
         <h2 className="text-2xl font-bold">100 Push-Ups Challenge</h2>
       </div>
@@ -21,4 +43,14 @@ export default function Browse() {
       </div>
     </DefaultLayout>
   );
+
 }
+
+
+export function clickHandler() {
+  console.log("test");
+  return (
+  console.log(<Program />)
+  )
+}
+
