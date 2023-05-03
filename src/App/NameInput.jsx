5a148aa2e52b27ce741be4a3e00925bcc312e90d
Jessica Hainke <j-k.hainke@t-online.de>
2023-05-03 07:35:58 +0200

    Highlight des Tages: Name-Input-Seite erstellt (Styling mau, aber egal) - es funktioniert auf jeden Fall :-)

---
+++ src/App/NameInput.jsx
@@ -0,0 +1,22 @@
+import DefaultLayout from "../Layouts/DefaultLayout";
+import { useRef } from 'react';
+
+
+export default function NameInput() {
+    
+    var nameInputRef = useRef();
+    console.log(nameInputRef);
+
+    function handleClick() {
+        console.log(nameInputRef.current.value)
+        nameInputRef.current.value = '';
+    }
+ 
+    return (
+    <DefaultLayout>
+        <p >Schön dass du dabei bist! Wie heißt du?</p>
+        <input placeholder="Dein Name" className="text-black" ref={nameInputRef}></input>
+        <button className="bg-red-500" onClick={handleClick}>TestButton</button>
+    </DefaultLayout>
+    )
+}
