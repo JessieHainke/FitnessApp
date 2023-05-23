5005620d14d0faac0c63bfd1cf3952038aa7aefb
Jessica Hainke <j-k.hainke@t-online.de>
2023-05-23 06:40:50 +0200

    Die mistige blöde Zeile 41 macht Ärger - da kommt immer der Error, dass die property of program as data as it is undefined - was soll das denn, ich hab doch alles so gemacht, wie es sein soll?

--- src/App/Workout.jsx
+++ src/App/Workout.jsx
@@ -1,44 +1,59 @@
 import { useQuery, gql } from "@apollo/client";
-
-import React from 'react';
-import Program from './Program';
-import ButtonsOrange from "./ButtonsOrange";
 import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
-import index from "./Browse";
-import Browse from "./Browse";
 
-    const PROGRAMS = gql`
-  query Programs {
-    programs {
+
+const PROGRAM = gql`
+  query Program($id: ID!) {
+    program(where: { id: $id }) {
       id
       name
+      description
       focus
       duration
       difficulty
+      image {
+        url
+      }
+      workouts {
+        id
+        duration
+        category
+      }
     }
-  }`;
+  }
+`;
+
   
 export default function Workout() {
 
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const { data, loading } = useQuery(PROGRAMS);
+  const { data, loading } = useQuery(PROGRAM, {
+    variables: { id },
+  });
  
+
   if (loading) {
     return <div>Loading...</div>
   } 
 
+  const { program } = data; // warum meckert er bei dieser blöden Zeile?!? Ich check das nicht... :-/
     return (
     <div className='bg-bgdark text-white h-screen w-screen'>
       <div className="flex pt-5 justify-center px-5">
-     {/*} {programs.map((program, index) => (<Link to="/default-workout"><h3 className="">{program.name}</h3></Link>))}
+      <button onClick={() => navigate(-1)} className="fixed top-5 right-5">
+        <img src="./img/arrowToProgram.svg"></img>
+      </button>
+     {/*} {programs.workouts.map((program, index) => (<Link to="/default-workout"><h3 className="">{program.name}</h3></Link>))}
         <Link to="/default-workout/"><img src="./img/arrowToProgram.svg" className="top-6 right-6 fixed" /></Link> */}
     </div>
         
         <div className="absolute inset-y-1/2 w-64 inset-x-1/4">
-            <h1 className="text-2xl">Tag 1</h1>
-            <p>26 Min. - Kraft und Koordination</p>
+            <h1 className="text-2xl">{program.name}</h1>
+            <p>{program.duration} Min. - Kraft und Koordination</p>
         </div>
-        <Link to="/default-workout" className="rounded-full bg-gradient-to-br from-orange to-pink px-12 py-2 flex justify-center mx-36 text-black">los!</Link>
+        <Link to={"/default-workout"} className="rounded-full bg-gradient-to-br from-orange to-pink px-12 py-2 flex justify-center mx-36 text-black">los!</Link>
     </div>
   )
 }
