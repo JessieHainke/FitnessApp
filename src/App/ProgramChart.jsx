5adf0cc1f26e9ff0b9d32a556a49c6d1939247bb
Jessica Hainke <j-k.hainke@t-online.de>
2023-05-29 07:11:50 +0200

    Program-Chart nach Vorbild des Workout-Charts angefangen, funktioniert noch nicht

---
+++ src/App/ProgramChart.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import ProgramPie from "./ProgramPie.jsx";
+import "./chartStyle.css";
+import "../index.css";
+
+
+
+export default function ProgramChart() {
+  const [random, setRandom] = useState({
+    percentage: 0,
+    colour: "hsl(0, 0%, 0%)",
+  });
+
+}
